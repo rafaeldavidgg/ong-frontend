8fe0addfd0364566943a11901c93f55a9ffe29b8
rafaeldavidgg <rafaeldgarciagalocha@gmail.com>
2025-04-06 12:59:53 +0200

    feat: eventos en calendario

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import { getActividadesPorUsuario } from "../services/actividadService";
+import { getEventosPorMes } from "../services/eventoService";
 import moment from "moment";
 import "moment/locale/es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -22,21 +23,35 @@ const Calendario = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
-    const fetchActividades = async () => {
+    const fetchDatos = async () => {
       const { actividades } = await getActividadesPorUsuario(usuarioId);
-      const eventosFormateados = actividades.map((act) => ({
+      const eventosActividades = actividades.map((act) => ({
         id: act._id,
         title: act.nombre,
         start: new Date(act.fecha),
         end: new Date(act.fecha),
         allDay: true,
+        tipo: "actividad",
       }));
-      setEventos(eventosFormateados);
+
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1;
+      const eventosDB = await getEventosPorMes(year, month);
+      const eventosEventos = eventosDB.map((ev) => ({
+        id: ev._id,
+        title: `${ev.nombre}`,
+        start: new Date(ev.fecha),
+        end: new Date(ev.fecha),
+        allDay: true,
+        tipo: "evento",
+      }));
+
+      setEventos([...eventosActividades, ...eventosEventos]);
       setLoading(false);
     };
 
-    fetchActividades();
-  }, [usuarioId]);
+    fetchDatos();
+  }, [usuarioId, currentDate]);
 
   const mensajes = {
     allDay: "Todo el día",
@@ -50,14 +65,14 @@ const Calendario = () => {
     date: "Fecha",
     time: "Hora",
     event: "Evento",
-    noEventsInRange: "No hay actividades en este rango",
+    noEventsInRange: "No hay actividades o eventos en este rango",
   };
 
   return (
     <div className="calendario-container">
       <Navbar />
       <div className="calendario-content">
-        <PageTitle title="Calendario de actividades" />
+        <PageTitle title="Calendario" />
         {loading ? (
           <LoadingScreen />
         ) : (
@@ -68,14 +83,37 @@ const Calendario = () => {
             endAccessor="end"
             style={{ height: 600 }}
             messages={mensajes}
-            eventPropGetter={() => ({
-              style: {
-                backgroundColor: "#104572",
-                color: "white",
+            eventPropGetter={(event) => {
+              const baseStyle = {
                 borderRadius: "8px",
-              },
-            })}
-            onSelectEvent={(evento) => navigate(`/actividades/${evento.id}`)}
+                color: "white",
+              };
+
+              if (event.tipo === "actividad") {
+                return {
+                  style: {
+                    ...baseStyle,
+                    backgroundColor: "#104572",
+                  },
+                };
+              } else if (event.tipo === "evento") {
+                return {
+                  style: {
+                    ...baseStyle,
+                    backgroundColor: "#E1B200",
+                  },
+                };
+              }
+
+              return {};
+            }}
+            onSelectEvent={(evento) => {
+              if (evento.tipo === "actividad") {
+                navigate(`/actividades/${evento.id}`);
+              } else if (evento.tipo === "evento") {
+                navigate(`/eventos/${evento.id}`);
+              }
+            }}
             views={["month", "week", "day", "agenda"]}
             view={currentView}
             onView={setCurrentView}
